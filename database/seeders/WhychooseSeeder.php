b3e92564b1d3141aba756b875e8687d1f03a3c40
unknown <ilhamfakih50@gmail.com>
2025-03-20 12:17:47 +0700

    update service ui

--- database/seeders/WhychooseSeeder.php
+++ database/seeders/WhychooseSeeder.php
@@ -22,29 +22,28 @@ class WhychooseSeeder extends Seeder
         $data = [
             [
                 'whychoose_id' => $why->id,
-                'title' => 'Experienced Workers ',
-                'description' => 'Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet ',
+                'title' => 'Tim Profesional dan Berpengalaman ',
+                'description' => 'Kami memiliki tim IT yang kompeten dan berpengalaman di bidang Network Operations Service, Development Application, dan Facility Operations Service. Setiap proyek ditangani dengan presisi dan keahlian terbaik.',
             ],
             [
                 'whychoose_id' => $why->id,
-                'title' => 'Reliable Industrial Services ',
-                'description' => 'Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet ',
+                'title' => 'Solusi IT yang Disesuaikan dengan Kebutuhan',
+                'description' => 'Kami tidak hanya menawarkan layanan standar, tetapi juga merancang solusi IT yang disesuaikan dengan kebutuhan unik bisnis Anda. Mulai dari pengembangan aplikasi hingga manajemen jaringan, kami memastikan solusi yang efisien dan efektif.',
             ],
             [
                 'whychoose_id' => $why->id,
-                'title' => '24/7 Customer Support ',
-                'description' => 'Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet ',
+                'title' => 'Dukungan 24/7 dan Responsif',
+                'description' => 'Layanan kami didukung oleh tim support yang siap membantu 24/7. Apapun kendala atau kebutuhan Anda, kami selalu siap memberikan solusi cepat dan tepat.',
             ],
             [
                 'whychoose_id' => $why->id,
-                'title' => 'Customer Support ',
-                'description' => 'Erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat ame ',
+                'title' => 'Harga Kompetitif dengan Kualitas Terbaik',
+                'description' => 'Kami menawarkan layanan IT berkualitas tinggi dengan harga yang kompetitif. Kami percaya bahwa teknologi canggih harus dapat diakses oleh semua bisnis, tanpa mengorbankan kualitas.',
             ],
         ];
 
         foreach ($data as $serviceData) {
             whychooseDetail::create($serviceData);
         }
-
     }
 }
